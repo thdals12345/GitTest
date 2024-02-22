d43e782e057d03f72dfe77df7b9a49d428e7cc5a
thdals12345 <thdals12345@naver.com>
2024-02-22 14:27:11 +0900

    ã집에서 픽작업

--- src/main/java/com/example/GitTest/TestController.java
+++ src/main/java/com/example/GitTest/TestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class TestController {
     @GetMapping("/")
     public String main(){
+        System.out.println(111111);
         return "main";
     }
 }
